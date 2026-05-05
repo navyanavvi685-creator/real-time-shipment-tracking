4416988034dee9d5066e92b90675de1f5403fd22
M Navya Sri <navyanavvi685@gmail.com>
2026-05-05 12:35:00 +0530

    feat: add custom exception for invalid shipment status transitions

---
+++ src/main/java/com/infotact/rstp/exception/InvalidShipmentStatusException.java
@@ -0,0 +1,7 @@
+package com.infotact.rstp.exception;
+
+public class InvalidShipmentStatusException extends RuntimeException {
+    public InvalidShipmentStatusException(String message) {
+        super(message);
+    }
+}
